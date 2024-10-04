2e0f9464b2bd2772b0dd79969ae9edca0243085b
racharron <rawleycharron@gmail.com>
2024-10-03 20:33:44 -0400

    Deleted unused files from when removing `loom` integration.

--- src/std_sync.rs
+++
@@ -1,5 +0,0 @@
-pub use std::sync::atomic::{AtomicBool, AtomicPtr, AtomicUsize, AtomicIsize, Ordering as AtomicOrd};
-pub use std::sync::{Arc, LockResult, Mutex, MutexGuard};
-pub use std::thread::{Builder, Thread, park, yield_now, JoinHandle, current};
-pub use std::sync::mpsc::channel;
-
